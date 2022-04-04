a34cea876eed37b27bb1a7fd3f465ae402aa0568
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-04-04 18:58:49 +0200

    Add support for board config files

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+from collections import defaultdict
 from models import Board
 from simulator import Simulation
 import os
+import json
+
+
+def get_grid(filename: str) -> defaultdict:
+    """Returns a defaultdict representing the grid of the board."""
+    grid = defaultdict(bool)
+    try:
+        with open(filename) as f:
+            board_config = json.load(f)
+            for cell in board_config:
+                # Split string into tuple
+                cell = tuple(int(x) for x in cell.split(","))
+                grid[cell] = True
+
+    except FileNotFoundError:
+        pass  # No config file found
+
+    return grid
 
 
 if __name__ == "__main__":
     width = os.get_terminal_size().columns
     height = os.get_terminal_size().lines
-    board = Board(width, height)
+    grid = get_grid("board_config.json")
 
-    board.grid[(4, 3)] = True
-    board.grid[(3, 4)] = True
-    board.grid[(4, 4)] = True
-    board.grid[(5, 4)] = True
-
-    simulation = Simulation(board=board)
+    board = Board(width, height, grid=grid)
+    simulation = Simulation(board=board, frame_time=0.1)
     simulation.run()
